99588e0994f7d30f49b99c8e8b3965fdf612defc
Manfred Touron <m@42.am>
2020-03-04 12:34:07 +0100

    chore: less code in main

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
+	if err := run(os.Args); err != nil {
+		log.Fatalf("error: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string) error {
 	fmt.Println("Hello World!")
+	return nil
 }

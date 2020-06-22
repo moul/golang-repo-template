60cfb7bdbd8963bcf7f5fa7d9d3af820d693d4fa
Manfred Touron <94029+moul@users.noreply.github.com>
2020-06-22 20:48:40 +0200

    chore: add goleak

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module moul.io/golang-repo-template
 go 1.13
 
 require (
+	go.uber.org/goleak v1.0.0
 	moul.io/motd v1.0.0
 	moul.io/srand v1.4.0
 )
--- main_test.go
+++ main_test.go
@@ -1,10 +1,19 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"go.uber.org/goleak"
+)
 
 func TestRun(t *testing.T) {
 	err := run(nil)
 	if err != nil {
 		t.Fatalf("err should be nil: %v", err)
 	}
+}
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+
 }
